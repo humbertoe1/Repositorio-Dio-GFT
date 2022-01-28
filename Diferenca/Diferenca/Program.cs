eac294f81d22ede5db0ff00009f5632982e3914d
humberto <humbertoe11@hotmail.com>
2022-01-28 17:53:27 -0300

    Primeiro desafio

---
+++ Diferenca/Diferenca/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Diferenca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D, X;
+
+            A = Convert.ToInt32(Console.ReadLine());
+            B = Convert.ToInt32(Console.ReadLine());
+            C = Convert.ToInt32(Console.ReadLine());
+            D = Convert.ToInt32(Console.ReadLine());
+
+            X = A * B - C * D;
+
+            Console.WriteLine($"DIFERENCA = {X}");
+        }
+    }
+}
