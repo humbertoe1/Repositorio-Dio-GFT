d73b74d6e1eeccb2058af70de5a144c89d057fbd
humbertoe1 <humbertoe11@hotmail.com>
2022-02-02 15:35:01 -0300

    Desafio quadrado e ao cubo

---
+++ AoQuadradoAndAoCubo/AoQuadradoAndAoCubo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AoQuadradoAndAoCubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] resposta = new int[2]; 
+
+            for (int i = 1; i <= n; i++)
+            {
+                resposta[0] = i * i;
+                resposta[1] = resposta[0] * i;
+                Console.WriteLine($"{i} {resposta[0]} {resposta[1]}");
+            }
+        }
+    }
+}
